49618faa534cf408821321f63dd0c2207e8abd5e
Rodrigo Penloski <r.penloski@gmail.com>
2015-10-21 07:40:56 -0200

    User case statement instead of if/elsif

--- lib/product.rb
+++ lib/product.rb
@@ -1,12 +1,13 @@
 class Product < Struct.new(:name, :price, :type)
 
   def subtotal(quantity)
-    if type == "CPM"
-      return (quantity / 1000) * price
-    elsif type == "CPC"
-      return quantity * price
+    case type
+    when "CPM"
+      (quantity / 1000) * price
+    when "CPC"
+      quantity * price
     else
-      return 1 * price
+      price
     end
   end
 
